09c8f142ef401845004f452d388933b25af2fad7
McNultyPT <mcnultypt@gmail.com>
2019-02-06 12:48:39 -0800

    writing action creator function

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,13 @@ export const FAILURE = 'FAILURE';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
-
+export const getCharacters = () => dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+        .get('https://swapi.co/api/people/')
+        .then(res => dispatch({ type: SUCCESS, payload: res.data }))
+        .catch(err => dispatch({ type: FAILURE, payload: err }));
+}
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
